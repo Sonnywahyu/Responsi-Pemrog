ba71297ab38b8adc62a86368f465666a03116c09
sonnywahyus <sonnywahyus@students.amikom.ac.id>
2022-04-19 11:53:59 +0700

    responsi Pemrog

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemograman4378
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==================================================");
+            Console.WriteLine("=========Responsi Pemograman Gaji Karyawan=======|");
+            Console.WriteLine("==================================================");
+            Console.WriteLine("Nama     : Sonny Wahyu Setiawan");
+            Console.WriteLine("NIM      : 21.11.4378");
+            Console.WriteLine("Kelas    : 21IF08");
+
+            Console.WriteLine("\n");
+
+            Karyawan karyawan1 = new Karyawan(2111, "Sonny", 4000000);
+            Karyawan karyawan2 = new Karyawan(1143, "Wahyu", 3000000);
+            Karyawan karyawan3 = new Karyawan(4378, "Setiawan", -2000000);
+
+            Console.WriteLine("==================================================");
+            Console.WriteLine(" NO | NIK | NAMA\t\t | Gaji Bulanan\t |");
+            Console.WriteLine("==================================================");
+            Console.WriteLine(" 1. | {0} {1} \t\t | {2}\t |", karyawan1.NIK, karyawan1.Nama, karyawan1.GajiBulanan);
+            Console.WriteLine(" 2. | {0} {1} \t\t | {2}\t |", karyawan2.NIK, karyawan2.Nama, karyawan2.GajiBulanan);
+            Console.WriteLine(" 3. | {0} {1} \t\t | {2}\t |", karyawan3.NIK, karyawan3.Nama, karyawan3.GajiBulanan);
+
+            Console.WriteLine("\n\nAnda Berhak Mendapatkan Kenaikan Gaji!\n");
+
+            Console.WriteLine("==================================================");
+            Console.WriteLine(" NO | NIK | NAMA\t\t | Gaji Bulanan\t |");
+            Console.WriteLine("==================================================");
+            Console.WriteLine(" 1. | {0} {1} \t\t | {2}\t |", karyawan1.NIK, karyawan1.Nama, karyawan1.NaikGaji);
+            Console.WriteLine(" 2. | {0} {1} \t\t | {2}\t |", karyawan2.NIK, karyawan2.Nama, karyawan2.NaikGaji);
+            Console.WriteLine(" 3. | {0} {1} \t\t | {2}\t |", karyawan3.NIK, karyawan3.Nama, karyawan3.GajiBulanan);
+
+            Console.ReadKey();
+        }
+    }
+
+}
